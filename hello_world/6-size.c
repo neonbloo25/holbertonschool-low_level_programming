85564f6b90571ef670bd0fd76545f121562f3f68
theluckdemon <theluckdemon@gmail.com>
2024-10-22 14:42:14 -0400

    lastly 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	printf("size of char: %zu bytes\n", sizeof(char));
 
-	printf("size of int: %zu bytes\n", sizeof(int));
+	printf("size of an int: %zu bytes\n", sizeof(int));
 
-	printf("size of long int: %zu bytes\n", sizeof(long int));
+	printf("size of a long int: %zu bytes\n", sizeof(long int));
 
-	printf("size of long long int: %zu bytes\n", sizeof(long long int));
+	printf("size of a long long int: %zu bytes\n", sizeof(long long int));
 
-	printf("size of float: %zu bytes\n", sizeof(float));
+	printf("size of a float: %zu bytes\n", sizeof(float));
 
 	return (0);
 }
