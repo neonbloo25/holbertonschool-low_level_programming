452c18dd8b59c409d396cd432b9f51d4554597e1
theluckdemon <theluckdemon@gmail.com>
2024-10-22 14:43:31 -0400

    lastly 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-	printf("size of char: %zu bytes\n", sizeof(char));
+	printf("Size of char: %zu bytes\n", sizeof(char));
 
-	printf("size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
 
-	printf("size of a long int: %zu bytes\n", sizeof(long int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int));
 
-	printf("size of a long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
 
-	printf("size of a float: %zu bytes\n", sizeof(float));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
 
 	return (0);
 }
