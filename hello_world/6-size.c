e937afcf429c59befdf96ecfb459898ba446764f
theluckdemon <theluckdemon@gmail.com>
2024-10-22 14:39:08 -0400

    lastly 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Printing various data sizes by type
+*
+* Return = 0 (Success)
+*/
+int main(void)
+{
+	printf("size of char: %zu bytes\n", sizeof(char));
+
+	printf("size of int: %zu bytes\n", sizeof(int));
+
+	printf("size of long int: %zu bytes\n", sizeof(long int));
+
+	printf("size of long long int: %zu bytes\n", sizeof(long long int));
+
+	printf("size of float: %zu bytes\n", sizeof(float));
+
+	return (0);
+}
