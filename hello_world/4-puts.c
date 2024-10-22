4be041a16ea6a9b5627a3011b2e5c60895870cb8
theluckdemon <theluckdemon@gmail.com>
2024-10-22 11:10:05 -0400

    onto 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing string w/ puts
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
