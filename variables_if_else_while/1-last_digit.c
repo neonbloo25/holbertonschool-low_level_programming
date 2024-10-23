4530e075b9503a955bbd30ce51d26830bc21e21c
theluckdemon <theluckdemon@gmail.com>
2024-10-23 14:41:01 -0400

    time for 1 again

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,23 +8,24 @@
  */
 int main(void)
 {
-	int n;
+	int n, lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
 
-	if (n > 5)
+	if (lastd > 5)
 	{
-		printf("Last digit of %d and is greater than 5\n", n);
+		printf("Last digit of %d and is greater than 5\n", lastd);
 	}
 
-	else if (n == 0)
+	else if (lastd == 0)
 	{
-		printf("Last digit of %d and is 0\n", n);
+		printf("Last digit of %d and is 0\n", lastd);
 	}
 	else
 	{
-		printf("Last digit of %d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d and is less than 6 and not 0\n", lastd);
 	}
 
 	return (0);
