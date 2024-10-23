cc1fb8f880f22cca63f5d59f6461d433eda18400
theluckdemon <theluckdemon@gmail.com>
2024-10-23 14:48:03 -0400

    time for 1 again

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf("Last digit of %d and is 0\n", lastd);
 	}
-	else if (lastd < 6)
+	else if (lastd < 6 && lastd != 0)
 	{
 		printf("Last digit of %d and is less than 6 and not 0\n", lastd);
 	}
