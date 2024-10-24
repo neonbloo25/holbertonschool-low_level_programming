d10da52972104640437fcf0269d84caf9e2ebcdf
theluckdemon <theluckdemon@gmail.com>
2024-10-23 21:37:03 -0400

    lets DO this

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Follow the pattern!
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+		if (number <= '8')
+		{
+			putchar(',');
+		}
+		else
+		{
+			putchar('$');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
