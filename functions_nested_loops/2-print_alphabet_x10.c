32082611deb498fcadaf119bb9fadc34e7d578bd
theluckdemon <theluckdemon@gmail.com>
2024-10-24 16:15:12 -0400

    onto 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - do i really have to tell u wat it do? rlly???
+ * Return: 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count;
+
+	count = 0;
+
+	while (count <= 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+		count++;
+	}
+}
