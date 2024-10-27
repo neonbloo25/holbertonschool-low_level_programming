fcffb53f92c0f49c4fc78eff94b9eae8b185aa41
theluckdemon <theluckdemon@gmail.com>
2024-10-27 07:39:47 -0400

    skip to 10, call it morbid curiosity

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - says it will just add two integer... i hope!
+ * @x: subject one
+ * @y: subject two
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+}
