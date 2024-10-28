38555a4462a6afd44f6e38ec901381ee6c4cc64f
theluckdemon <theluckdemon@gmail.com>
2024-10-28 13:54:29 -0400

    count with m3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
  * print_numbers - teaches us how to count
  */
 
 void print_numbers(void)
 {
-	int num = 0;
+	char num = '0';
 
-	while (num < 9)
+	while (num <= '9')
 	{
 		_putchar(num);
 		num++;
 	}
+	_putchar ('\n');
 }
