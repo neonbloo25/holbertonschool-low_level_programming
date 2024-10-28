eca05da16fd548ac17b4072ff73bf8898813b4d5
theluckdemon <theluckdemon@gmail.com>
2024-10-28 12:50:35 -0400

    Onto 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,15 @@
-#include <stdlib.h>
-
-\**\
-
-int main(void)
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isdigit - check wether its a number or not
+ * @c: subject in question
+ *
+ * Return: 1 or 0 i
+ */
+int _isdigit(int c)
 {
-    return (0);
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
 }
