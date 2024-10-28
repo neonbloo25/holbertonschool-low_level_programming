2da0a446b3ee3aee4f554c2b5c39e27843ace044
theluckdemon <theluckdemon@gmail.com>
2024-10-28 12:03:03 -0400

    re Zero(not the anime)

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-
-\**\
-
-int main(void)
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isupper - verifies if the subject is upper case
+ * @c: is the subject in question
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int c)
 {
-    return (0);
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
 }
