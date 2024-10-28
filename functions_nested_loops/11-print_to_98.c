88481f726260e2d43c26faaa23973fcb9a7dce0c
theluckdemon <theluckdemon@gmail.com>
2024-10-27 21:42:41 -0400

    Zoom to 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - does as says on label
+ * @n: is the number
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
