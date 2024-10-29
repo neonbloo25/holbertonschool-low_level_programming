ac59a56e0cbba0cd2030da591f99fe4cf626a60b
theluckdemon <theluckdemon@gmail.com>
2024-10-29 13:33:20 -0400

    re seven

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,25 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
-\**\
+/**
+ * print_diagonal - prints diagonal line
+ * @n: measurement of line
+ */
 
-int main(void)
+void print_diagonal(int n)
 {
-    return (0);
+	int x, y;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y <= x; y++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
