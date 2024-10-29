f2cf8b421e0a479f3cfabf34ade03d215b9376b6
theluckdemon <theluckdemon@gmail.com>
2024-10-29 14:05:26 -0400

    OURcraft

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,10 +10,15 @@ void print_square(int size)
 {
 	int x, y;
 
-	for (x = 0; x < size; x++)
-	{
-		for (y = 0; y <= size - 1; y++)
-			_putchar('#');
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y <= size - 1; y++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
