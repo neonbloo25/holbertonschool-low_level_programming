ed338ea6709b87f7db367486265f3634d9faec23
theluckdemon <theluckdemon@gmail.com>
2024-10-29 15:47:21 -0400

    Fizz Buzz FIZZBUZZ./9-fizz_buzz!

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,33 @@
-#include <stdlib.h>
+#include <stdio.h>
 
-\**\
+/**
+ * main - will do a classic Fizz Buzz! Fizz(3) Buzz(5) FizzBuzz(3&5)
+ * Return: 0
+ */
 
 int main(void)
 {
-    return (0);
+	int fbcount;
+
+	for (fbcount = 1; fbcount <= 100; fbcount++)
+	{
+		if (((fbcount % 3) == 0) && ((fbcount % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if ((fbcount % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((fbcount % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d", fbcount);
+		if (fbcount != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
