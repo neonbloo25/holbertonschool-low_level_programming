c827566bd6e7085c7feba00f2be54ed021f34bb4
theluckdemon <theluckdemon@gmail.com>
2024-10-29 19:27:04 -0400

    to 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,25 @@
-#include <stdlib.h>
-
-\**\
-
-int main(void)
+#include "main.h"
+/**
+ * print_triangle - makes a weird square
+ * @size: how big da twiangle will be
+ */
+void print_triangle(int size)
 {
-    return (0);
+	int x;
+	int y;
+	int z;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= (size - x); y++)
+				_putchar(' ');
+			for (z = 1; z <= x; z++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
 }
