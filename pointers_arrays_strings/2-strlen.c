edcb19447229ee52e1c76438f0cdb65e86eff621
theluckdemon <theluckdemon@gmail.com>
2024-10-30 14:44:42 -0400

    re 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,17 @@
 #include "main.h"
-/**/
-
+/**
+ * _strlen - checks length of string
+ * @s: is the string in question!
+ * Return: length of string
+ */
+int _strlen(char *s)
 {
+	int length = 0;
+
+	while (*s != '\0')
+		{
+		s++;
+		length++;
+		}
+	return (length);
 }
