4d24acd50402734db848f0a26b5d13e8aab47b0a
theluckdemon <theluckdemon@gmail.com>
2024-10-30 13:35:38 -0400

    to 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,14 @@
 #include "main.h"
-/**/
-
+/**
+ * swap_int - swaps values of two ints
+ * @a: swaps with b
+ * @b: swaps with a
+ */
+void swap_int(int *a, int *b)
 {
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }
