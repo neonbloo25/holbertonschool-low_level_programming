ba2b3882364340bd53c5248208ccf01614f4fc80
theluckdemon <theluckdemon@gmail.com>
2024-10-30 17:46:44 -0400

    to 4 (six eight...)

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,18 @@
 #include "main.h"
-/**/
-
+/**
+ * print_rev - prints string in reverse
+ * @s: is string
+ */
+void print_rev(char *s)
 {
+		int i;
+
+		for (i = 0; s[i] != '\0'; i++)
+			continue;
+		while (i != 0)
+		{
+			_putchar(s[i]);
+			--i;
+		}
+		_putchar('\n');
 }
