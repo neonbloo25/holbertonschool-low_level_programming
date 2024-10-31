12f128b61b9fc6079bf6be2c587a178c89276a5d
theluckdemon <theluckdemon@gmail.com>
2024-10-31 12:14:18 -0400

    to 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,22 @@
 #include "main.h"
-/**/
-
+/**
+ * rev_string - reverses string in question
+ * @s: is string
+ */
+void rev_string(char *s)
 {
+	int i;
+	int x;
+	int length;
+	int arr;
+
+	for (i = 0; s[i] != '\0'; i++)
+		{}
+	length = i;
+	for (x = 0; i > length / 2; i--, x++)
+		{
+		arr = s[i - 1];
+		s[i - 1] = s[x];
+		s[x] = arr;
+		}
 }
