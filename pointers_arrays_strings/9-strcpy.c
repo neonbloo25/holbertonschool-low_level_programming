de0c0c4414feeeeeba8f78118f913527a0482756
theluckdemon <theluckdemon@gmail.com>
2024-11-01 09:49:16 -0400

    to 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,15 @@
 #include "main.h"
-/**/
-
+/**
+ * _strcpy - copies string from one p/char to aother
+ * @dest: desired destination for text
+ * @src: initial string
+ * Return: dest pointer
+ */
+char *_strcpy(char *dest, char *src)
 {
+	int i;
+
+	for (i = 0; src[i] <= '\0'; i++)
+		dest[i] = src[i + 1];
+	return (dest);
 }
