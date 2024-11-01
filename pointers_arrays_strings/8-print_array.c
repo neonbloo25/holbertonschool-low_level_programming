fc99ad8e1242d0f33d1e35ef799577ad9ed395d7
theluckdemon <theluckdemon@gmail.com>
2024-11-01 09:26:43 -0400

    8oom!

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,19 @@
 #include "main.h"
-/**/
-
+#include <stdio.h>
+/**
+ * print_array - prints a whole array of ints
+ * @a: Array in question
+ * @n: Number of slots in array
+ */
+void print_array(int *a, int n)
 {
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+		if (i == (n - 1))
+			printf("%d", a[i]);
+	printf("\n");
 }
