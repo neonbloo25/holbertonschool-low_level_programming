b378624b086cae4412381f3a212a666a83010130
theluckdemon <theluckdemon@gmail.com>
2024-11-02 23:31:05 -0400

    to 1./1-strncat!

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - will link two strings together
+ * @dest: string 1
+ * @src: string 2
+ * Returns: the resulting dest string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i,x;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+
+	x = 0;
+	while (x < n && src[x] != '\0')
+	{
+		dest[i] = src[x];
+		i++;
+		x++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
