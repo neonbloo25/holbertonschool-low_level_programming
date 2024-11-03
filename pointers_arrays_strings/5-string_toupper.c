bc1f844987683a5e93d8abc40a6fd4cfeb2b2d78
theluckdemon <theluckdemon@gmail.com>
2024-11-03 15:52:57 -0400

    to 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - lowercase letters to upper case
+ * @word: word to switch to characters
+ * Return: Oops all caps!
+ */
+char *string_toupper(char *word)
+{
+	int i = 0;
+	int swap = 'a' - 'A';
+
+	for ( i = 0; word[i] != '\0' ; ++i)
+	{
+		if (word[i] >= 'a' && word[i] <= 'z')
+		{
+			word[i] = word[i] - swap;
+		}
+	}
+	return (word);
+}
