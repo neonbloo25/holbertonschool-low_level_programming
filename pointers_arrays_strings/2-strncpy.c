9c2ab679b0453531db672280ff7b870c8385f406
theluckdemon <theluckdemon@gmail.com>
2024-11-03 12:32:26 -0400

    fixed two lines, retry 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,12 +15,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (i < n && src[i] != '\0')
 	{
+		dest [i] = src[i];
 		i++;
 	}
 
 	while (i < n)
 	{
-		dest[i] = src[i];
+		dest[i] = '\0';
 		i++;
 	}
 
