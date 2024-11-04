9132b478e0f543960a6ee90dbed916a6312793d4
theluckdemon <theluckdemon@gmail.com>
2024-11-04 15:07:05 -0400

    0 m g please...

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,4 +1,16 @@
 #include "main.h"
 /**
+ * _memset - rewrites n amount of s with b
+ * @s: subject string
+ * @b: desired char
+ * @n: intended length of rewrite
+ * Return: altered string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
-{}
+	for (i = 0; i <= n; i++)
+		s[i] = b;
+	return (s);
+}
