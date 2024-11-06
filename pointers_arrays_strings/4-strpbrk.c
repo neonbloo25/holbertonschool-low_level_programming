0dde54d7b42519c5f08809e25c6deeb36e59c566
theluckdemon <theluckdemon@gmail.com>
2024-11-06 09:50:10 -0400

    to 4(six eight)

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,25 @@
+#include <stddef.h>
 #include "main.h"
 /**
+ * _strpbrk - does thing
+ * @s: the string
+ * @accept: the thing
+ * Return: modded *s, likely
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, x;
 
-{}
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		for (x = 0; accept[x] != '\0'; x++)
+		{
+			if (s[i] == accept[x])
+				return (&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
