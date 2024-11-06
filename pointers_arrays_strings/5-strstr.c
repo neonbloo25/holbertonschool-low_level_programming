f88e5bd2b380da3271cfbf2d4e088641c6318711
theluckdemon <theluckdemon@gmail.com>
2024-11-06 11:46:36 -0400

    omg plz 5...

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,26 @@
 #include "main.h"
+#include <stddef.h>
 /**
+ * _strstr - scours through string to find match if any
+ * @haystack: string to browse
+ * @needle: search match criteria
+ * Return: pointer aimed at first match or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, x;
 
-{}
+	i = 0;
+
+	while (haystack[i] != '\0')
+	{
+		for (x = 0; haystack[i] == needle[x]; x++)
+			i++;
+
+		if (needle[x] == '\0')
+			return (&haystack[i - x]);
+
+		i++;
+	}
+	return (NULL);
+}
