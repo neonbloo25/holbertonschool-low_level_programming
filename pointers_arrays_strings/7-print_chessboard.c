ba2bdfe7706085eb76d3887719d7120a47d5ab70
theluckdemon <theluckdemon@gmail.com>
2024-11-06 14:00:31 -0400

    to 7(thinks its 6)

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,20 @@
 #include "main.h"
 /**
+ * print_chessboard - U WANT CHESSS?!?!?! U GOT IT BBY!!!!!
+ * @a: chess array bbyyyyyyyyy
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, x;
+	char t;
 
-{}
+	for (i = 0; i <= 7; i++)
+	{
+		for (x = 0; x <= 7; x++)
+		{
+			t = a[i][x];
+			_putchar(t);
+		}
+		_putchar('\0');
+	}
+}
