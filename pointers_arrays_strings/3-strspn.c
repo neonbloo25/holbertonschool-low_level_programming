46ae017cdf5087b15bb83f212fb5be9c9ceee442
theluckdemon <theluckdemon@gmail.com>
2024-11-06 02:48:06 -0400

    to 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,27 @@
 #include "main.h"
 /**
+ * _strspn - locates specific chars
+ * @s: target to search
+ * @accept: search term(s)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
 
-{}
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+			{
+				bytes++;
+				break;
+			}
+			else if ((accept[i]) == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
