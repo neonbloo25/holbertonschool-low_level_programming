d2dbf731fce22ed9ec157686976512e303efd4c2
theluckdemon <theluckdemon@gmail.com>
2024-11-07 11:15:51 -0400

    Starting from 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,16 @@
 #include "main.h"
-/**/
-
+/**
+ * _puts_recursion - prints string, and a newline
+ * @s: target string
+ */
+void _puts_recursion(char *s)
 {
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
