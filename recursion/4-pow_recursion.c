2205f41d808cab50c9e85339c47815123cc6b055
theluckdemon <theluckdemon@gmail.com>
2024-11-07 16:02:08 -0400

    to 4(six eight)

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,5 +1,15 @@
 #include "main.h"
-/**/
-
+/**
+ * _pow_recursion - returns X to the power of y
+ * @x: base factor
+ * @y: terget vector
+ * Return: result
+ */
+int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
