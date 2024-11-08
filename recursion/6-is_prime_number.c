bf81113544775ece9c49bada66672f67190939ab
theluckdemon <theluckdemon@gmail.com>
2024-11-08 11:14:14 -0400

    TO SIX./6-prime !

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,28 @@
 #include "main.h"
-/**/
-
+/**
+ * prime_check - checks if number is prime
+ * @n: target num
+ * @x: cross ref param
+ * Return: 1 or 0
+ */
+int prime_check(int x, int n)
 {
+	if (n < 1)
+		return (0);
+	else if (x * x > n && n != 1)
+		return (1);
+	else if (x * x == n || n <= 1)
+		return (0);
+	return (prime_check(x + 1, n));
+}
+/**
+ * is_prime_number - checks if prime
+ * @n: target number
+ * Return: result
+ */
+int is_prime_number(int n)
+{
+	int x = 1;
+
+	return (prime_check(x, n));
 }
