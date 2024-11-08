3fbd7143d4df420bb447012ede1f6fc956679af7
theluckdemon <theluckdemon@gmail.com>
2024-11-08 10:25:00 -0400

    to 5./5-sqrt !

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,26 @@
 #include "main.h"
-/**/
-
+/**
+ * square_check - finds if target has square root
+ * @x: comparison parameter
+ * @n: target int
+ * Return:square root if any, else, error
+ */
+int square_check(int x, int n)
 {
+	if (x * x == n)
+		return (x);
+	if (x * x > n)
+		return (-1);
+	return (square_check(x + 1, n));
+}
+/**
+ * _sqrt_recursion - Returns square root
+ * @n: target int
+ * Return:square root if any, else, error
+ */
+int _sqrt_recursion(int n)
+{
+	int x = 1;
+
+	return (square_check(x, n));
 }
