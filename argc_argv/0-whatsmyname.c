522dfd33ecf872d8817c44848974a8e6ff8c9ee7
theluckdemon <theluckdemon@gmail.com>
2024-11-09 15:08:10 -0400

    let's g0./mynameis !

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints filename
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: well i HAD to give argc something to do!
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (argc);
+}
