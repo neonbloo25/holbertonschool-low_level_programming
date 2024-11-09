4bd2da1c8da1a52d0b133cb6a9ea5d343cd20000
theluckdemon <theluckdemon@gmail.com>
2024-11-09 15:42:48 -0400

    to 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - displays argument count
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	if (argv[0] == NULL || *argv[0] == '\0')
+		return (-1);
+	return (0);
+}
