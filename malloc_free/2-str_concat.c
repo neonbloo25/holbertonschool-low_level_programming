181a794a1489e3b3cece2037527481d67e87174d
theluckdemon <theluckdemon@gmail.com>
2024-11-11 15:23:16 -0400

    tutu

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,32 @@
+#include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
-/**/
-int main()
+/**
+ * str_concat - concatenates two strings as one
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer with result
+ */
+char *str_concat(char *s1, char *s2)
 {
+	char *x;
+	int i, d, n;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{}
+
+	for (d = 0; s2[d] != '\0'; d++)
+	{}
+
+	x = malloc(i * sizeof(*x) + 1);
+
+	for (n = d; n <= i + d; n++)
+	{
+		x[d] = s2[n - i];
+		n++;
+	}
+	return (x);
 }
