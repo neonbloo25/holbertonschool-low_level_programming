99a43d03ee4831df303dafc3ee5d3ce4d97bf5d9
theluckdemon <theluckdemon@gmail.com>
2024-11-11 15:02:33 -0400

    to 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,5 +1,29 @@
+#include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
-/**/
-int main()
+/**
+ * _strdup - copies from pointer to pointer
+ * @str: the strangggg
+ * Return: te pointer bby
+ */
+char *_strdup(char *str)
 {
+	char *a;
+	int i, c;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{}
+
+	a = malloc(i * sizeof(*a) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
+
+	return (a);
 }
