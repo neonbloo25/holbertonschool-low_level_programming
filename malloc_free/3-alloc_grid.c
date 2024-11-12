73f3ef5dc8109d41831ad52a2013229492056aad
theluckdemon <theluckdemon@gmail.com>
2024-11-12 12:59:21 -0400

    to 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,5 +1,46 @@
+#include <stddef.h>
+#include <stdlib.h>
 #include "main.h"
-/**/
-int main()
+/**
+ * alloc_grid - makes 2d array
+ * @width: primary vector
+ * @height: secondary vector
+ * Return: Pointer to arr2d on success, else Null
+ */
+int **alloc_grid(int width, int height)
 {
+	int **PiARR, x, i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	PiARR = (int **)malloc(sizeof(int *) * height);
+
+	if (PiARR != NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			PiARR[i] = (int *)malloc(sizeof(int) * width);
+			if (PiARR[i] != NULL)
+			{
+				for (x = 0; x < width; x++)
+					PiARR[i][x] = 0;
+			}
+			else
+			{
+				while (i >= 0)
+				{
+					free(PiARR[i]);
+					i--;
+				}
+				free(PiARR);
+				return (NULL);
+			}
+		}
+		return (PiARR);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
