1e4ed3d0985d9493a91273e9d7fc5425efc1c0f7
theluckdemon <theluckdemon@gmail.com>
2024-11-12 13:31:21 -0400

    re 4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,5 +1,16 @@
+#include <stdlib.h>
 #include "main.h"
-/**/
-int main()
+/**
+ * free_grid - purge array of sin
+ * @grid: sinner
+ * @height: sins
+ * Return: zero.
+ */
+void free_grid(int **grid, int height)
 {
+	int i;
+
+	for (i = 0; grid[i] < grid[height]; i++)
+		free(grid[i]);
+	free(grid);
 }
