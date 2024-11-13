fa5c027a2c142fe3a89674db04fde0a4aa8c0048
theluckdemon <theluckdemon@gmail.com>
2024-11-13 10:52:16 -0400

    lets begin with 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,19 @@
+#include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
-//
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: amount to reserve
+ * Return: pointer to memory
+ */
 void *malloc_checked(unsigned int b)
 {
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
 }
