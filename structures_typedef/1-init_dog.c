1a14b7ba011b9933eff2067430702f0857f6677b
theluckdemon <theluckdemon@gmail.com>
2024-11-14 12:01:48 -0400

    to 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include <stddef.h>
-#include "main.h"
 #include "dog.h"
 /**
  * init_dog - initializes a variable of the created dog struct type
  * struct dog - secondary header containing structs
+ * @d: struct label
  * @name: char type var
  * @age: int type var
  * @owner: char type var
@@ -11,14 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog d;
-	d = malloc(sizeof(struct dog);
 	if (d == NULL)
-		return (NULL);
+		return;
 
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-
-	return (d);
 }
