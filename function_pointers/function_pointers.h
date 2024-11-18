8571d80962c0fd560bb8d8c81a686776fabbf799
theluckdemon <theluckdemon@gmail.com>
2024-11-18 14:24:27 -0400

    file namefix

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+/**
+ * New header, new functions ft. _putchar
+ */
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
