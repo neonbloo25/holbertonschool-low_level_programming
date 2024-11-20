2f8d81c9bd8a49f015bb4d6f74b067712512f303
theluckdemon <theluckdemon@gmail.com>
2024-11-20 15:25:17 -0400

    push all 4 3- files

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -38,6 +38,7 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
+		printf("Error\n");
 		exit(100);
 
 	return (a / b);
@@ -51,6 +52,7 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
+		printf("Error\n");
 		exit(100);
 
 	return (a % b);
