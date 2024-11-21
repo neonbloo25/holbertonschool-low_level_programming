dd536233b521f571ff460b663e3a44ad0bddb74a
theluckdemon <theluckdemon@gmail.com>
2024-11-21 15:51:37 -0400

    to 3

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings
+ * @separator: string to be used between strings
+ * @n: number of string passed to function
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list printSs;
+	unsigned int i;
+	char *print_strings;
+
+	va_start(printSs, n);
+	for (i = 0; i < n; i++)
+	{
+		print_strings = va_arg(printSs, char *);
+		printf("%s", print_strings);
+
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(printSs);
+	printf("\n");
+}
