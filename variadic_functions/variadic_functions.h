95cf3a03dc7c7c621cfff6daa1cd6d504ec3a1ed
theluckdemon <theluckdemon@gmail.com>
2024-11-21 14:25:22 -0400

    re header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,13 +2,9 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdio.h>
 #include <stdarg.h>
-/**
- * Prototype lines
- * _putchar - the man of the hour
- * @c: n his lovely wife
- * Return: stufffffffff
- */
+
 int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
