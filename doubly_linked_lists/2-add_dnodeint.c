9c0f9d11609cb4da5132d87c19a454eb40b5e84b
theluckdemon <theluckdemon@gmail.com>
2024-12-07 16:53:58 -0400

    to 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,17 +9,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *xyz;
 
-	if (!head)
-		return (NULL);
-
 	xyz = malloc(sizeof(dlistint_t));
 	if (xyz == NULL)
 		return (NULL);
 
 	xyz->n = n;
-	xyz->prev = NULL;
-	xyz->next = NULL;
+	xyz->next = *head;
 	*head = xyz;
 
-	return (xyz);
+	return (*head);
 }
