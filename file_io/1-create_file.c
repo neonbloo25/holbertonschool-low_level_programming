fd6486144eee14a82e81a44edbaf247b18dfc87f
theluckdemon <theluckdemon@gmail.com>
2024-12-12 03:37:48 -0400

    to 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * create_file - makes and fills file
+ * @filemane: name of subject
+ * @text_content: it's filling
+ * Return: 1/-1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int len, x, i;
+
+	len = 0, x = 0, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (x == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+	}
+	else
+	{
+		close(x);
+		return(1);
+	}
+
+	i = write(x, text_content, len);
+	if (i == -1 || i != len)
+		return (-1);
+
+	close(x);
+	return (1);
+}
