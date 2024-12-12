04b268109e19af0b4933c91fd5c0ae9ac517b3b8
theluckdemon <theluckdemon@gmail.com>
2024-12-12 03:50:56 -0400

    to 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,89 @@
+#include "main.h"
+char *create_buffer(char *file);
+void close_file(int fd);
+/**
+ * create_buffer - makes space for buffer.
+ * @file: file label.
+ * Return: ptr to buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+/**
+ * close_file - Closes file?
+ * @fd: file description
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int o, x, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	o = open(argv[1], O_RDONLY);
+	r = read(0, buffer, 1024);
+	x = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (o == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(x, buffer, r);
+		if (x == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(o, buffer, 1024);
+		x = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(o);
+	close_file(x);
+
+	return (0);
+}
